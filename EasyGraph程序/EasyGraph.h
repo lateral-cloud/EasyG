1d8083c95e52b5bec589bc3731dbb13382b9f4b7
侧云 <12063261+lateral-cloud@user.noreply.gitee.com>
2023-01-30 08:10:18 +0000

    删除文件 EasyGraph程序

--- EasyGraph程序/EasyGraph.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "EasyGraph/EasyGraphDef.h"
-#include "EasyGraph/EasyMessage.h"
-#include "EasyGraph/EasyBitmap.h"
-#include "EasyGraph/EasyWindow.h"
-#include "EasyGraph/EasyPaint.h"
-#include "EasyGraph/EasyDrawWindow.h"
--- EasyGraph程序/EasyGraph/EasyMessage.cpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "EasyMessage.h"
-#include <windowsx.h>
