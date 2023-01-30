9ffc37fb52c30da2a1fc65b1c54e4abbf7432eec
侧云 <12063261+lateral-cloud@user.noreply.gitee.com>
2023-01-30 08:11:18 +0000

    EasyG 头文件
    Signed-off-by: 侧云 <12063261+lateral-cloud@user.noreply.gitee.com>

---
+++ EasyG/EasyG.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "EasyG/EasyGDef.h"
+#include "EasyG/EasyGHead.h"
+
+#include "EasyG/EasyColor.h"
+#include "EasyG/EasySize.h"
+#include "EasyG/EasyMessage.h"
+#include "EasyG/EasyPattern.h"
+#include "EasyG/EasyFlag.h"
+
+#include "EasyG/EasyHDC.h"
+#include "EasyG/EasyImage.h"
+#include "EasyG/EasyCanvas.h"
+#include "EasyG/EasyLayer.h"
+#include "EasyG/EasyScene.h"
+#include "EasyG/EasyWindow.h"
